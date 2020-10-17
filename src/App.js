e34ec9df1617810a3b2c70e1f209860c9caa9f96
Joe Cox <joecox@live.co.uk>
2020-10-17 01:29:34 +0100

    added react helmet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Helmet from "react-helmet";
 import Sidebar from "./components/Sidebar/Sidebar";
 import "./App.scss";
 import Main from "./components/Main/Main";
@@ -6,6 +7,9 @@ import Main from "./components/Main/Main";
 const App = () => {
   return (
     <div className="wrapper">
+      <Helmet>
+        <title>Workflow</title>
+      </Helmet>
       <Sidebar />
       <Main />
     </div>
