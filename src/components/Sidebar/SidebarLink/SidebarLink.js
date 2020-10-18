af3159b1a48deb45ab2eb10f736c1f3b5fa91d68
Joe Cox <joecox@live.co.uk>
2020-10-18 18:14:30 +0100

    fixed issue with logo scaling on sidebar open. fixed flashing svg icon issue on sidebar open. fixed sidebar link new line showing on sidebar open by adding container with no overflow around the text element

--- src/components/Sidebar/SidebarLink/SidebarLink.js
+++ src/components/Sidebar/SidebarLink/SidebarLink.js
@@ -3,9 +3,18 @@ import styles from "./SidebarLink.module.scss";
 
 const SidebarLink = ({ children, text, slim }) => {
   return (
-    <a href="/" className={styles.sidebar_link}>
-      {children}{" "}
-      {slim ? null : <span className={styles.sidebar_link__text}>{text}</span>}
+    <a
+      href="/"
+      className={`${styles.sidebar_link} ${
+        slim ? styles.sidebar_link__slim : ""
+      }`}
+    >
+      {children}
+      {slim ? null : (
+        <p className={styles.sidebar_link__text_container}>
+          <span className={styles.sidebar_link__text}>{text}</span>
+        </p>
+      )}
     </a>
   );
 };
